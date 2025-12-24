51a5de02616582c62caaf55846577115182b8a88
milesmercer04 <milesmercer04@gmail.com>
2025-12-24 14:16:58 -0800

    Started revising directory tree for frontend folder to make rough drafts of student lecture and challenge views

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import StudentLectureView from "./views/StudentLectureView";
+import StudentChallengeView from "./views/StudentChallengeView";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mode, setMode] = useState("lecture");  // "lecture" | "challenge"
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {mode === "lecture" && <StudentLectureView onSwitch={() => setMode("challenge")} />}
+      {mode === "challenge" && <StudentChallengeView onSwitch={() => setMode("lecture")} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
